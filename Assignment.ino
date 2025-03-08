647090afa45c78c0cbdf0c1fa4f76f57d89531e5
chandrakiran.reddy <chandrakiran.reddy@npci.org.in>
2025-03-08 15:14:19 +0530

    This is a fake commit

--- Assignment.ino
+++ Assignment.ino
@@ -8,7 +8,7 @@ they are:
 timer 0 is 8 bit counter count 0 - 255.
 timer 1 is 16 bit counter counts 0 - 65600.
 timer 2 is 8 bit counter same as timer 0.
-
+/*app
 These timer are depend on real time clock. If we handle them rigth we can will no - negligible error in real time.
 In this program I have used timer0 with preScaler value of 1/64.
 */
